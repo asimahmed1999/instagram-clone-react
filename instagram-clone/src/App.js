c0980b5e03d5ae1d996404109d5e392c7d0258df
asimahmed1999 <asimahmed1999@gmail.com>
2021-10-03 10:48:11 +0500

    Database connected

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,8 +1,19 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import React from 'react';
 import Post from './Post';
+import { db } from './firebase';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  
+  // useEffect -> Runs a piece of code based on a specific condition
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      setPosts(snapshot.docs.map(doc => doc.data()));
+    })
+  }, []);
+
   return (
     <div className="app">
       
@@ -17,9 +28,19 @@ function App() {
 
       {/* {Header} */}
       <h1>Hello World!</h1>
-      <Post username="Asim Ahmed" caption=" Wow it works" imageUrl="https://www.freecodecamp.org/news/content/images/2021/06/Ekran-Resmi-2019-11-18-18.08.13.png"/>
+
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        ))
+      }
+
+
+
+
+      {/* <Post username="Asim Ahmed" caption=" Wow it works" imageUrl="https://www.freecodecamp.org/news/content/images/2021/06/Ekran-Resmi-2019-11-18-18.08.13.png"/>
       <Post username="Saturo Gojo" caption=" You're weak. xD" imageUrl="https://i.pinimg.com/736x/8e/de/53/8ede538fcf75a0a1bd812810edb50cb7.jpg" />
-      <Post username="eren yeager" caption=" AoT" imageUrl="https://i.pinimg.com/originals/b2/8e/88/b28e88c761df7aa9e54e19be3439b596.jpg" />
+      <Post username="eren yeager" caption=" AoT" imageUrl="https://i.pinimg.com/originals/b2/8e/88/b28e88c761df7aa9e54e19be3439b596.jpg" /> */}
       {/* {Posts} */}
       {/* {Posts} */}
     </div>
